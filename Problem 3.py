2df7275a88e4abd87eeead5c1da3ca2c4dc69635
Joseph Pecoraro <jpecoraro342@ufl.edu>
2014-12-02 10:53:52 -0500

    Restructured Problem Directory

--- Problem 3.py
+++ Problem 3.py
@@ -1,25 +1,29 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
 number_to_factorize = 600851475143
 largest_factor = -1
 
 i = 2
-primeFound = False
+prime_found = False
 
 while i < number_to_factorize:
 	if number_to_factorize%i == 0:
 		factor = number_to_factorize/i
 		print str(factor) + " and " + str(i) + " Is a factor of " + str(number_to_factorize) 
+
 		if (factor < 17425170):
-			isPrime = True
+			is_prime = True
 			for j in range(2, factor):
 				if factor % j == 0:
-					isPrime = False
+					is_prime = False
 					break
 			
-			if isPrime:
+			if is_prime:
 				largest_factor = factor
-				primeFound = True
+				prime_found = True
 	
-	if primeFound:
+	if prime_found:
 		break
 	i = i+1
 	
